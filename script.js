eb3c57c4c47b3500a6d3cbdc384b69f7992c5eb8
Luiza Carneiro <luizaccarneiro@gmail.com>
2025-03-21 15:43:24 +0000

    scroll

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+//scroll-anima
+
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+const elements = document.querySelectorAll(".hidden");
+elements.forEach((element) => {
+  myObserver.observe(element);
+});
+
+//menu-mobile
+
+const { download } = require("express/lib/response");
+
 const menuMobile = document.querySelector(".container-menu-hamburguer");
 const btnMobile = document.querySelector("[data-menu='button']");
 
