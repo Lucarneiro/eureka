c165c0c0e98470425b3fcdb918ba51ab522e8477
Luiza Carneiro <luizaccarneiro@gmail.com>
2025-04-10 17:26:12 +0100

    js

---
+++ script.js
@@ -0,0 +1,56 @@
+//galeria
+
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.getElementById("imageModal");
+  const modalImg = document.getElementById("modalImage");
+  const closeBtn = document.querySelector(".close");
+
+  // Seleciona todas as imagens da galeria
+  document.querySelectorAll(".gallery__img").forEach((img) => {
+    img.addEventListener("click", function () {
+      modal.style.display = "block";
+      modalImg.src = this.src;
+    });
+  });
+
+  // Fechar modal ao clicar no "X"
+  closeBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  // Fechar modal ao clicar fora da imagem
+  modal.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
+
+//scroll-anima
+
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+const elements = document.querySelectorAll(".hidden");
+elements.forEach((element) => {
+  myObserver.observe(element);
+});
+
+//menu-mobile
+document.addEventListener("DOMContentLoaded", function () {
+  const menuMobile = document.querySelector(".container-menu-hamburguer");
+  const btnMobile = document.querySelector("[data-menu='button']");
+
+  function openMenu(event) {
+    menuMobile.classList.toggle("active");
+    btnMobile.classList.toggle("active");
+  }
+
+  btnMobile.addEventListener("click", openMenu);
+});
